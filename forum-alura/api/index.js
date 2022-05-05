b86423c7aab8c36afc53cfd2b0fdf31063f71942
Margarida <margaridammcm@gmail.com>
2022-05-04 22:33:54 -0300

    Autenticando API

--- forum-alura/api/index.js
+++ forum-alura/api/index.js
@@ -1,10 +1,23 @@
 const Koa = require('koa') //lib que facilita criacao de apis dentro do nodejs //chamando classe do Koa
+const conversor = require('basic-auth')
 const processador = require('koa-bodyparser')
 const aplicacao = new Koa() //instanciando classe do Koa
 const pesquisar = require('../bigQuery/pesquisar')
 
 aplicacao.use(processador())
 aplicacao.use(async function(contexto){ //gerenciando requisicao dentro da api //contexto é o objeto que contem requisição e resposta
+    const usuarioEsenha = conversor.parse(contexto.request.headers.authorization)
+    console.log(usuarioEsenha)
+    const USUARIO = process.env.USUARIO
+    const SENHA = process.env.SENHA
+    
+    if(USUARIO !== usuarioEsenha.name || SENHA !== usuarioEsenha.pass) {
+        contexto.body = {
+            mensagem: 'Acesso negado!'
+        }
+        return
+    }
+
     const corpoDaRequisicao = contexto.request.body
     contexto.status = 200
     contexto.body = await pesquisar(corpoDaRequisicao.filtro)
