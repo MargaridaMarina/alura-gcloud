90b1e280692a577aebd9eba9b1062b152dd15730
Margarida <margaridammcm@gmail.com>
2022-05-04 22:08:40 -0300

    API aceitando filtros

--- forum-alura/api/index.js
+++ forum-alura/api/index.js
@@ -1,10 +1,13 @@
 const Koa = require('koa') //lib que facilita criacao de apis dentro do nodejs //chamando classe do Koa
+const processador = require('koa-bodyparser')
 const aplicacao = new Koa() //instanciando classe do Koa
 const pesquisar = require('../bigQuery/pesquisar')
 
+aplicacao.use(processador())
 aplicacao.use(async function(contexto){ //gerenciando requisicao dentro da api //contexto é o objeto que contem requisição e resposta
+    const corpoDaRequisicao = contexto.request.body
     contexto.status = 200
-    contexto.body = await pesquisar()
+    contexto.body = await pesquisar(corpoDaRequisicao.filtro)
 })
 
 aplicacao.listen(3000)
